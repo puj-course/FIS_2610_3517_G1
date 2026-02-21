ae6cee1669a72e3fa15dde784aa3d70f132f7b8e
Naqz05 <nataliaquinonezz@gmail.com>
2026-02-20 19:42:16 -0500

    feat: Función para calcular un hash de una contraseña y comparar entre dos hashes (#27)

--- backend/auth.py
+++ backend/auth.py
@@ -1,3 +1,18 @@
 # Subissue - Encriptar contraseñas
 #import section 
-import cryptography
+import hashlib
+
+def hash_password(password): #firma de la función
+
+    salt        =   b'some_salt' #salt para agregar seguridad a la contraseña
+    
+    pwd_salt    =   password+salt.decode("utf-8")  
+
+    digest      =   hashlib.sha256(pwd_salt.encode())
+
+    return digest.hexdigest()
+
+
+def verify_password_hash(password, reference_hash):
+
+    return hash_password(password) == reference_hash

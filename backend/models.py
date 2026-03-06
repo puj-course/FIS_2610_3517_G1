76fe8753855ae93ac9e39a15c651238b3f470f2e
vanexalram <vanesaa_ramos@javeriana.edu.co>
2026-03-05 23:34:07 -0500

    HU-02: Diseño del modelo de paciente implementado
    Closes #120
    Closes #121
    Closes #122
    Closes #123

--- backend/models.py
+++ backend/models.py
@@ -1,34 +1,21 @@
 import os
-<<<<<<< HEAD
-# libreia liviana que estamos usando
-=======
->>>>>>> develop
 import sqlite3
 
 # Esto asegura que la BD siempre quede en backend/database.db
 DB_PATH = os.path.join(os.path.dirname(__file__), "database.db")
 
-<<<<<<< HEAD
 # Funcion para abrir una conexión con la BD
-=======
->>>>>>> develop
 def get_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
-<<<<<<< HEAD
 # Funcion para inicializar la BD
-=======
->>>>>>> develop
 def init_db():
     conn = get_connection()
     cursor = conn.cursor()
 
-<<<<<<< HEAD
     # Tabla de usuarios
-=======
->>>>>>> develop
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS usuarios (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -38,38 +25,23 @@ def init_db():
             rol TEXT NOT NULL CHECK(rol IN ('cuidador', 'administrador'))
         )
     """)
-
-<<<<<<< HEAD
+   # solo 2 subissues estan aqui
     # Tabla de pacientes
-=======
->>>>>>> develop
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS pacientes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nombres TEXT NOT NULL,
             apellidos TEXT NOT NULL,
-<<<<<<< HEAD
             fecha_nacimiento TEXT NOT NULL,
-=======
-            fecha_nacimiento TEXT NOT NULL, -- mm/dd/yyyy
->>>>>>> develop
             genero TEXT NOT NULL,
             tipo_documento TEXT NOT NULL,
             numero_documento TEXT NOT NULL,
             telefono_contacto TEXT NOT NULL,
-<<<<<<< HEAD
-            eps_aseguradora TEXT NOT NULL,
-            diagnostico_principal TEXT NOT NULL,
-            alergias_conocidas TEXT DEFAULT '',
-            observaciones_adicionales TEXT DEFAULT '',
-            UNIQUE(tipo_documento, numero_documento)
-=======
             eps_aseguradora TEXT,
             diagnostico_principal TEXT,
             alergias_conocidas TEXT,
             observaciones_adicionales TEXT,
             UNIQUE (tipo_documento, numero_documento)
->>>>>>> develop
         )
     """)
 
@@ -79,5 +51,6 @@ def init_db():
 
 if __name__ == "__main__":
     init_db()
-
+    # aumentara la implementacion
     
+

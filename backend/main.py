d4c0184c8bbb834442c5ebd55c516052f1b86d02
rodriguezdiazandres8 <rodriguezdiaz.andres8@gmail.com>
2026-03-25 00:34:20 -0500

    test: pruebas funcionales del modulo de recordatorios
    Closes #284
    Closes #285
    Closes #286
    Closes #287
    Closes #288

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,9 @@
-# main.py del backend
-
 from fastapi import FastAPI
-from backend.routes.patient_route import router as patient_router # Registro de patient_route
-
-## Para el end point de medicamentos
+from backend.routes.patient_route import router as patient_router
 from backend.routes.medication_route import router as medication_router
-
-# para el endpoint de recordatorios
 from backend.routes.reminder_route import router as reminder_router
 
 app = FastAPI()
-
 
 app.include_router(patient_router)
 app.include_router(medication_router)

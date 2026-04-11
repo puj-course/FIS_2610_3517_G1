b1c3a671f1da2644448001b98d172c20c8d2409b
Naqz05 <nataliaquinonezz@gmail.com>
2026-04-10 21:27:12 -0500

    feat: crear la clase concreta EstadoPendiente Closes #386

--- backend/states/estado_toma.py
+++ backend/states/estado_toma.py
@@ -7,3 +7,7 @@ class EstadoToma(ABC):
     #ahora esta funcion lo que hace es devolver el nombre del estado actual de la toma de medicamentos como pendiente, tomada o asi
     def tomar_medicamento(self)->str:
         pass    
+    class EstadoPendiente(EstadoToma):
+        #estado de una toma cuya hora programada aun no ha llegado o lleva menos de 60 minutos de retraso sin ser marcada
+        def tomar_medicamento(self) -> str:
+            return "pendiente"

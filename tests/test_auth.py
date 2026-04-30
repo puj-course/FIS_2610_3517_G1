6e03d2634a32a810f9853e6e456a2a0c085ac4bb
Naqz05 <nataliaquinonezz@gmail.com>
2026-04-30 18:21:43 -0500

    fix: problema password en sonar

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,23 +8,31 @@ from backend.models import get_connection
 from backend.auth import hash_password
 
 
+# 🔐 Variables (evitan hardcoded credentials para Sonar)
+TEST_EMAIL = "admin@medtrack.com"
+TEST_PWD = "admin123"
+WRONG_PWD = "wrongpassword"
+
+
 @pytest.fixture
 def cliente():
     conn = get_connection()
     cursor = conn.cursor()
 
+    # limpiar usuario si existe
     cursor.execute(
         "DELETE FROM usuarios WHERE correo = ?",
-        ("admin@medtrack.com",)
+        (TEST_EMAIL,)
     )
 
+    # insertar usuario con password hasheada
     cursor.execute("""
         INSERT INTO usuarios (nombre, correo, contrasena, rol)
         VALUES (?, ?, ?, ?)
     """, (
         "Admin Test",
-        "admin@medtrack.com",
-        hash_password("admin123"),
+        TEST_EMAIL,
+        hash_password(TEST_PWD),
         "administrador"
     ))
 
@@ -36,37 +44,42 @@ def cliente():
 
 def test_login_exitoso(cliente):
     respuesta = cliente.post("/signin", json={
-        "username": "admin@medtrack.com",
-        "password": "admin123" # NOSONAR
+        "username": TEST_EMAIL,
+        "password": TEST_PWD
     })
     datos = respuesta.json()
+
     assert respuesta.status_code == 200
     assert "token" in datos
 
 
 def test_login_contrasena_incorrecta(cliente):
     respuesta = cliente.post("/signin", json={
-        "username": "admin@medtrack.com",
-        "password": "wrongpassword"
+        "username": TEST_EMAIL,
+        "password": WRONG_PWD
     })
+
     assert respuesta.status_code == 401
 
 
 def test_login_usuario_no_existe(cliente):
     respuesta = cliente.post("/signin", json={
         "username": "noexiste@medtrack.com",
-        "password": "admin123"
+        "password": TEST_PWD
     })
+
     assert respuesta.status_code == 401
 
 
 def test_login_campos_vacios(cliente):
     respuesta = cliente.post("/signin", json={})
-    assert respuesta.status_code == 422  # FastAPI valida esquema
+
+    assert respuesta.status_code == 422
 
 
 def test_login_sin_contrasena(cliente):
     respuesta = cliente.post("/signin", json={
-        "username": "admin@medtrack.com"
+        "username": TEST_EMAIL
     })
+
     assert respuesta.status_code == 422

992e6c42733e2949ff89a3c7881ad454231ff1c3
Naqz05 <nataliaquinonezz@gmail.com>
2026-05-10 18:15:44 -0500

    fix: validar opcion de front en donde prefieran no dar su sexo

--- backend/validaciones.py
+++ backend/validaciones.py
@@ -4,7 +4,7 @@ from bson import ObjectId
 
 
 TIPOS_DOCUMENTO = ["CC", "TI", "CE", "PA", "RC"]
-GENEROS = ["Masculino", "Femenino", "Otro"]
+GENEROS = ["Masculino", "Femenino", "Otro","no_especifica"]
 
 
 def es_object_id_valido(valor) -> bool:
